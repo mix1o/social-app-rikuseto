6e2bb80cb33d90963d402ce66dfdaa37a827ca96
Nightmare9254 <adrian.sarna59@gmail.com>
2021-07-23 13:36:44 +0200

    Backgeound image for auth pages for >=tablet; changed display for menu

---
+++ src/components/Navigation/Menu.tsx
@@ -0,0 +1,20 @@
+const Menu = () => {
+  return (
+    <nav className="menu">
+      <div>
+        <i className="fas fa-bell"></i>
+      </div>
+      <div>
+        <i className="fas fa-bell"></i>
+      </div>
+      <div>
+        <i className="fas fa-plus"></i>
+      </div>
+      <div>
+        <i className="fas fa-comment-dots"></i>
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
