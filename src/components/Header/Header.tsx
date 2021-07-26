4a24543b7f4d9e9190da174e9a9cf60219ff605e
Nightmare9254 <adrian.sarna59@gmail.com>
2021-07-26 17:59:41 +0200

    testing menu animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,26 @@ import { useCookies } from 'react-cookie';
 import SignIn from '../Auth/SignIn';
 import SignUp from '../Auth/SignUp';
 import { motion as m, AnimatePresence as Presence } from 'framer-motion';
-import { set } from 'lodash';
+
+import { useRef } from 'react';
 
 const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0, x: '100%' },
+  open: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeIn',
+    },
+  },
+  closed: {
+    opacity: 0,
+    x: '100%',
+    transition: {
+      type: 'tween',
+      ease: 'easeOut',
+    },
+  },
 };
 
 const HeaderItem = () => {
@@ -19,7 +34,7 @@ const Header: FC = () => {
   const [cookies, , removeCookie] = useCookies();
   const { user } = cookies;
   const [userOption, setUserOption] = useState<number>(0);
-
+  const menuRef = useRef<HTMLButtonElement>(null);
   const handleUserChoose = (option: number) => {
     setUserOption(option);
   };
@@ -28,60 +43,64 @@ const Header: FC = () => {
     setUserOption(0);
     setOpenMenu(false);
   }, [user]);
-  console.log(openMenu);
+
   return (
     <div className="header">
       <h3 className="header__message">RS</h3>
       <button
         className="header__nav-button"
         onClick={() => setOpenMenu(!openMenu)}
+        // ref={menuRef}
       ></button>
-      <Presence>
-        {openMenu && (
-          <m.div
-            initial={{ x: '100%' }}
-            animate={{ x: 0 }}
-            transition={{ type: 'tween' }}
-            exit={{ x: 1000 }}
-            className="header__menu"
-          >
-            <div onClick={() => setOpenMenu(false)}>x</div>
-            {!user && (
-              <div>
-                <button
-                  onClick={() => {
-                    handleUserChoose(1);
-                    setOpenMenu(false);
-                  }}
-                >
-                  Log In
-                </button>
-                <button
-                  onClick={() => {
-                    setOpenMenu(false);
-                    handleUserChoose(2);
-                  }}
-                >
-                  Sign In
-                </button>
-              </div>
-            )}
-            {user && (
-              <div>
-                <button onClick={() => removeCookie('user')}>Log out</button>
-                <button>Account</button>
-              </div>
-            )}
-          </m.div>
+      <m.div
+        initial="closed"
+        animate={openMenu ? 'open' : 'closed'}
+        variants={variants}
+        className="header__menu"
+        // drag
+        // dragConstraints={menuRef}
+        // onDrag={e => console.log(e)}
+      >
+        <div onClick={() => setOpenMenu(false)}>x</div>
+        {!user && (
+          <div>
+            <button
+              onClick={() => {
+                handleUserChoose(1);
+                setOpenMenu(false);
+              }}
+            >
+              Log In
+            </button>
+            <button
+              onClick={() => {
+                handleUserChoose(2);
+                setOpenMenu(false);
+              }}
+            >
+              Sign In
+            </button>
+          </div>
         )}
-      </Presence>
+        {user && (
+          <div>
+            <button onClick={() => removeCookie('user')}>Log out</button>
+            <button>Account</button>
+          </div>
+        )}
+      </m.div>
       <div style={{ position: 'absolute', top: 0, left: 0 }}>
         {userOption === 1 && (
           <>
             <SignIn /> <button onClick={() => setUserOption(0)}>close</button>
           </>
         )}
-        {userOption === 2 && <SignUp />}
+        {userOption === 2 && (
+          <>
+            {' '}
+            <SignUp /> <button onClick={() => setUserOption(0)}>close</button>
+          </>
+        )}
       </div>
     </div>
   );
