ae8eb24deb0ae0de5655149ada33918091f395a0
Nightmare9254 <adrian.sarna59@gmail.com>
2021-07-26 13:08:51 +0200

    Basic hamburger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,21 @@
-import React, { FC, useState, useEffect } from 'react';
-import styled from 'styled-components';
+import { FC, useState, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import SignIn from '../Auth/SignIn';
 import SignUp from '../Auth/SignUp';
+import { motion as m, AnimatePresence as Presence } from 'framer-motion';
+import { set } from 'lodash';
 
-const UserAuthContainer = styled.div`
-  position: fixed;
-  right: 0;
-  top: 0;
-  background: #fff;
-  width: 100%;
-  min-height: 300px;
-  text-align: center;
-`;
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '100%' },
+};
+
+const HeaderItem = () => {
+  return <div>a</div>;
+};
 
 const Header: FC = () => {
-  const [openAuth, setOpenAuth] = useState<boolean>(false);
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
   const [cookies, , removeCookie] = useCookies();
   const { user } = cookies;
   const [userOption, setUserOption] = useState<number>(0);
@@ -26,38 +26,63 @@ const Header: FC = () => {
 
   useEffect(() => {
     setUserOption(0);
-    setOpenAuth(false);
+    setOpenMenu(false);
   }, [user]);
-
+  console.log(openMenu);
   return (
     <div className="header">
-      {user && <p>{user.first_name}</p>}
-      {!user && <p>You are not logged in</p>}
+      <h3 className="header__message">RS</h3>
       <button
-        style={{ background: 'transparent', border: 'none' }}
-        onClick={() => setOpenAuth(true)}
-      >
-        <i className="fas fa-user"></i>
-      </button>
-      {openAuth && (
-        <UserAuthContainer>
-          <div onClick={() => setOpenAuth(false)}>x</div>
-          {!user && (
-            <>
-              <button onClick={() => handleUserChoose(1)}>Log In</button>
-              <button onClick={() => handleUserChoose(2)}>Sign In</button>
-              {userOption === 1 && <SignIn />}
-              {userOption === 2 && <SignUp />}
-            </>
-          )}
-          {user && (
-            <div>
-              <button onClick={() => removeCookie('user')}>Log out</button>
-              <button>Account</button>
-            </div>
-          )}
-        </UserAuthContainer>
-      )}
+        className="header__nav-button"
+        onClick={() => setOpenMenu(!openMenu)}
+      ></button>
+      <Presence>
+        {openMenu && (
+          <m.div
+            initial={{ x: '100%' }}
+            animate={{ x: 0 }}
+            transition={{ type: 'tween' }}
+            exit={{ x: 1000 }}
+            className="header__menu"
+          >
+            <div onClick={() => setOpenMenu(false)}>x</div>
+            {!user && (
+              <div>
+                <button
+                  onClick={() => {
+                    handleUserChoose(1);
+                    setOpenMenu(false);
+                  }}
+                >
+                  Log In
+                </button>
+                <button
+                  onClick={() => {
+                    setOpenMenu(false);
+                    handleUserChoose(2);
+                  }}
+                >
+                  Sign In
+                </button>
+              </div>
+            )}
+            {user && (
+              <div>
+                <button onClick={() => removeCookie('user')}>Log out</button>
+                <button>Account</button>
+              </div>
+            )}
+          </m.div>
+        )}
+      </Presence>
+      <div style={{ position: 'absolute', top: 0, left: 0 }}>
+        {userOption === 1 && (
+          <>
+            <SignIn /> <button onClick={() => setUserOption(0)}>close</button>
+          </>
+        )}
+        {userOption === 2 && <SignUp />}
+      </div>
     </div>
   );
 };
--- src/components/Navigation/Menu.tsx
+++ src/components/Navigation/Menu.tsx
@@ -1,18 +1,110 @@
+import { FC, useState, useMemo } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+interface MenuProps {
+  className: string;
+  href: string;
+  position: number;
+  currentPosition: number;
+}
+
+const MenuItem: FC<MenuProps> = ({
+  className,
+  href,
+  position,
+  currentPosition,
+}) => {
+  return (
+    <Link to={href} className="menu__item">
+      <i
+        className={`${className} ${
+          currentPosition === position ? 'menu__item--active' : null
+        }`}
+      ></i>
+    </Link>
+  );
+};
+
+// const Menu: FC = () => {
+//   const location = useLocation();
+//   const [position, setPosition] = useState(0);
+//   useMemo(() => {
+//     switch (location.pathname) {
+//       case '/':
+//         setPosition(0);
+//         break;
+//       case '/notification':
+//         setPosition(1);
+//         break;
+//       case '/new-post':
+//         setPosition(2);
+//         break;
+//       case '/messages':
+//         setPosition(3);
+//         break;
+//       case '/auth':
+//         setPosition(4);
+//         break;
+
+//       default:
+//         console.log('null');
+//     }
+//   }, [location]);
+
+//   return (
+//     <nav className="menu">
+//       <MenuItem
+//         className="fas fa-home"
+//         href="/"
+//         position={0}
+//         currentPosition={position}
+//       />
+//       <MenuItem
+//         className="fas fa-bell"
+//         href="/notification"
+//         position={1}
+//         currentPosition={position}
+//       />
+//       <MenuItem
+//         className="fas fa-plus"
+//         href="/new-post"
+//         position={2}
+//         currentPosition={position}
+//       />
+//       <MenuItem
+//         className="fas fa-comment-dots"
+//         href="/messages"
+//         position={3}
+//         currentPosition={position}
+//       />
+//       <MenuItem
+//         className="fas fa-user"
+//         href="/auth"
+//         position={4}
+//         currentPosition={position}
+//       />
+//     </nav>
+//   );
+// };
+
 const Menu = () => {
   return (
     <nav className="menu">
-      <div>
-        <i className="fas fa-bell"></i>
-      </div>
-      <div>
-        <i className="fas fa-bell"></i>
-      </div>
-      <div>
-        <i className="fas fa-plus"></i>
-      </div>
-      <div>
-        <i className="fas fa-comment-dots"></i>
-      </div>
+      <Link to="/">
+        <i className={`fas fa-home menu__item `}></i>
+      </Link>
+      <Link to="/notification">
+        <i className={`fas fa-bell menu__item `}></i>
+      </Link>
+      <Link to="/new-post">
+        <i className={`fas fa-plus menu__item `}></i>
+      </Link>
+      <Link to="/messages">
+        <i className={`fas fa-comment-dots menu__item `}></i>
+      </Link>
+      <Link to="/auth">
+        <i className={`fas fa-user menu__item `}></i>
+      </Link>
     </nav>
   );
 };
