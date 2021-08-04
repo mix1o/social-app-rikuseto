22d0ca3d11acb55eff09e272c3993566340cf022
Mikolaj <smgpwwe@wp.pl>
2021-08-04 13:43:07 +0200

    Change js to tsx tests

---
+++ src/components/Posts/__CreatePost__/CreatePost.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import CreatePost from '../CreatePost';
+import { fireEvent, render, cleanup, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+let getByTestId: any;
+
+beforeEach(() => {
+  const component = render(
+    <CreatePost handleFetchPosts={() => {}} setOpen={() => {}} />
+  );
+  getByTestId = component.getByTestId;
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+test('header renders with correct text', () => {
+  const header = getByTestId('create-post-header');
+
+  expect(header.textContent).toBe('Create new post');
+});
+
+test('button renders with disabled', () => {
+  const button = getByTestId('button');
+
+  expect(button.hasAttribute('disabled'));
+});
+
+test('input correctly change value', () => {
+  const headline = getByTestId('headline');
+
+  expect(headline.value).toBe('');
+
+  fireEvent.change(headline, {
+    target: {
+      value: 'New post',
+    },
+  });
+
+  expect(headline.value).toBe('New post');
+});
+
+test('message displaying correctly', async () => {
+  const headline = getByTestId('headline');
+
+  fireEvent.change(headline, {
+    target: {
+      value: 'Something',
+    },
+  });
+
+  const message = getByTestId('message');
+
+  await waitFor(() => expect(message).toBeInTheDocument());
+});
