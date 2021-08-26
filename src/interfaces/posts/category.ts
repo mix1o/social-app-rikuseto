1fc73f6822987e11e2986d4a6ddfe7ec2cfbfdf4
Nightmare9254 <adrian.sarna59@gmail.com>
2021-08-26 18:42:11 +0200

    Changed file from tsx to ts, exporting interfaces for post categories

---
+++ src/interfaces/posts/category.ts
@@ -0,0 +1,22 @@
+import { Dispatch, SetStateAction } from 'react';
+import { CreatePostI } from './postInterfaces';
+
+export interface CategoryArray {
+  _id: string;
+  name: string;
+  totalPosts: number;
+  user_id: string;
+}
+
+export interface CategoryProps {
+  chooseCategory?: string;
+  setPost: React.Dispatch<React.SetStateAction<CreatePostI>>;
+  post: CreatePostI;
+}
+
+export interface CategoryItemI {
+  name: string;
+  totalPosts?: number;
+  handleSetCategory: (value: string) => void;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
