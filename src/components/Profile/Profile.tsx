485acb0ab0ca8126c72977894b9b68b887da09fe
Mikolaj <smgpwwe@wp.pl>
2021-08-30 15:53:52 +0200

    Component profile, fetching data for user - personal infromation, posts, comments

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,142 @@
+import axios from 'axios';
+import React, { FC, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { CommentsData } from '../../interfaces/comments/commentsInterfaces';
+import { PostInterface } from '../../interfaces/posts/postInterfaces';
+import Header from '../Header/Header';
+import Post from '../Posts/Post';
+import Comment from '../Comments/Comment/Comment';
+
+interface UserDataInterface {
+  id: string;
+  avatar: string;
+  firstName: string;
+  lastName: string;
+  date: string;
+}
+
+interface ProfileI {
+  user: UserDataInterface;
+  posts: PostInterface[];
+  comments: CommentsData[];
+}
+
+const Profile: FC = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const [profile, setProfile] = useState<ProfileI>();
+  const [option, setOption] = useState<string>('posts');
+
+  const MODE_POSTS = 'posts';
+  const MODE_COMMENTS = 'comments';
+
+  const getUserData = () => {
+    axios.get(`${process.env.REACT_APP_API}/profile?userId=${id}`).then(res => {
+      setProfile(res.data);
+    });
+  };
+
+  const formatDate = () => {
+    let combineDate;
+
+    if (profile?.user) {
+      let date = new Date(profile!.user!.date);
+
+      let year = date.getFullYear();
+      let month: string | number = date.getMonth() + 1;
+      let day: string | number = date.getDate();
+
+      if (day < 10) day = `0${day}`;
+      if (month < 10) month = `0${month}`;
+
+      combineDate = `${day}/${month}/${year}`;
+      return combineDate;
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+  return (
+    <>
+      <Header />
+      <div className="profile">
+        <div className="profile__user">
+          <img
+            src={profile?.user?.avatar}
+            className="profile__image"
+            alt="Avatar"
+          />
+          <p className="profile__name">
+            {profile?.user.firstName} {profile?.user.lastName}
+          </p>
+          <div className="profile__info">
+            <p className="profile__date">Joined: {formatDate()}</p>
+            <p className="profile__total">
+              Total posts: {profile?.posts.length}
+            </p>
+          </div>
+        </div>
+        <div className="profile__choice">
+          <button
+            className={`profile__btn ${
+              option === MODE_POSTS ? 'profile__btn--active' : ''
+            }`}
+            onClick={() => setOption(MODE_POSTS)}
+          >
+            Posts
+          </button>
+          <button
+            className={`profile__btn ${
+              option === MODE_COMMENTS ? 'profile__btn--active' : ''
+            }`}
+            onClick={() => setOption(MODE_COMMENTS)}
+          >
+            Comments
+          </button>
+        </div>
+        {option === MODE_POSTS && (
+          <div>
+            {profile?.posts?.map(
+              ({ _id, headline, category, file, user_id, likes, date }) => {
+                return (
+                  <Post
+                    key={_id}
+                    _id={_id}
+                    headline={headline}
+                    category={category}
+                    file={file}
+                    user_id={user_id}
+                    likes={likes}
+                    refreshPosts={getUserData}
+                    date={date}
+                  />
+                );
+              }
+            )}
+          </div>
+        )}
+        {option === MODE_COMMENTS && (
+          <div className="profile__comments">
+            {profile?.comments.map(({ _id, text, user_id, likes, date }) => {
+              return (
+                <Comment
+                  key={_id}
+                  _id={_id}
+                  text={text}
+                  user_id={user_id}
+                  likes={likes}
+                  date={date}
+                  refreshComments={getUserData}
+                  fetchTopComment={() => {}}
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Profile;
