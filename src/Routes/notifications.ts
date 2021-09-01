b424a238a6c6f2aea9bea35b2e9c718c692c1bee
Mikolaj <smgpwwe@wp.pl>
2021-09-01 16:43:39 +0200

    Route for notifications

---
+++ src/Routes/notifications.ts
@@ -0,0 +1,7 @@
+import Notifications from '../components/UserNotifications/Notifications';
+
+export const NotificationRoutes = {
+  component: Notifications,
+  url: '/notification',
+  exact: true,
+};
