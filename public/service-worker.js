af7837d29c4057bb5950f3bcd1e1476b9cf2d42e
Nightmare9254 <adrian.sarna59@gmail.com>
2021-09-02 13:08:20 +0200

    added example option data, moved push handler outside of if statment

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,20 +7,37 @@ const receivePushNotification = e => {
   });
   const { title, text, url } = e.data.json();
 
+  //EG
+  // const options = {
+  //   data: url,
+  //   body: text,
+  //   icon: image,
+  //   vibrate: [200, 100, 200],
+  //   tag: tag,
+  //   image: image,
+  //   badge: 'https://spyna.it/icons/favicon.ico',
+  //   actions: [
+  //     {
+  //       action: 'Detail',
+  //       title: 'View',
+  //       icon: 'https://via.placeholder.com/128/ff0000',
+  //     },
+  //   ],
+  // };
+
   const options = {
+    data: url,
     body: text,
     vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2',
-    },
+
     actions: [
       {
-        action: 'explore',
-        title: 'DIS',
+        action: 'Detail',
+        title: 'Check it out',
         // icon: 'images/checkmark.png',
       },
-      { action: 'close', title: 'Close' },
+      // #TODO Cancel notification
+      { action: 'Close', title: 'Close' },
     ],
   };
   e.waitUntil(self.registration.showNotification(title, options));
