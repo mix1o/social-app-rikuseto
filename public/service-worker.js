0fbd11643f7a7c0105a21109f23729d08906135c
Nightmare9254 <adrian.sarna59@gmail.com>
2021-09-02 12:06:31 +0200

    removed sw from index.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,64 +1,29 @@
-const CACHE = 'V1';
-const urls = ['index.html', 'offline.html'];
-
 const self = this;
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll(urls);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('offline.html'));
-    })
-  );
-});
-
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE);
-
-  e.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (!cacheWhitelist.includes(name)) caches.delete(name);
-          return null;
-        })
-      )
-    )
-  );
-});
 
 const receivePushNotification = e => {
   self.clients.matchAll().then(c => {
     if (c.length === 0) {
-      const { title, text, url } = e.data.json();
-
-      const options = {
-        body: text,
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: '2',
-        },
-        actions: [
-          {
-            action: 'explore',
-            title: 'DIS',
-            // icon: 'images/checkmark.png',
-          },
-          { action: 'close', title: 'Close' },
-        ],
-      };
-      e.waitUntil(self.registration.showNotification(title, options));
     }
   });
+  const { title, text, url } = e.data.json();
+
+  const options = {
+    body: text,
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2',
+    },
+    actions: [
+      {
+        action: 'explore',
+        title: 'DIS',
+        // icon: 'images/checkmark.png',
+      },
+      { action: 'close', title: 'Close' },
+    ],
+  };
+  e.waitUntil(self.registration.showNotification(title, options));
 };
 
 const openPushNotification = event => {
