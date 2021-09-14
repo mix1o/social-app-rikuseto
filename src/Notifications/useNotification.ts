d665830c8237fa07916ac9b99cbb21458d43d492
Mikolaj <smgpwwe@wp.pl>
2021-09-14 17:03:26 +0200

    Removed unused code

--- src/Notifications/useNotification.ts
+++ src/Notifications/useNotification.ts
@@ -14,7 +14,6 @@ const checkNotificationSupport = notificationSupport();
 const useNotification = () => {
   const [userPermission, setUserPermission] = useState(Notification.permission);
   const [userSubscription, setUserSubscription] = useState<any>();
-  const [pushServerId, setPushServerId] = useState();
   const [error, setError] = useState<any>(false);
   const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState('');
@@ -104,55 +103,10 @@ const useNotification = () => {
       });
   };
 
-  // //GARBAGE
-  // const onClickSendSubscriptionToPushServer = async () => {
-  //   setLoading(true);
-  //   setError(false);
-
-  //   fetch(`${process.env.REACT_APP_API}/subscription`, {
-  //     credentials: 'omit',
-  //     headers: {
-  //       'content-type': 'application/json;charset=UTF-8',
-  //       'sec-fetch-mode': 'cors',
-  //     },
-  //     body: JSON.stringify(userSubscription),
-  //     method: 'POST',
-  //     mode: 'cors',
-  //   })
-  //     .then((res: any) => {
-  //       setPushServerId(res!.id);
-  //       console.log(res.id);
-  //     })
-  //     .catch(err => {
-  //       setLoading(false);
-  //       setError(err);
-  //     });
-  // };
-
-  // const onClickSendNotification = async () => {
-  //   setLoading(true);
-  //   setError(false);
-
-  //   fetch(`${process.env.REACT_APP_API}/subscription/${pushServerId}`, {
-  //     credentials: 'omit',
-  //     headers: {
-  //       'content-type': 'application/json;charset=UTF-8',
-  //       'sec-fetch-mode': 'cors',
-  //     },
-  //     method: 'GET',
-  //     mode: 'cors',
-  //   }).catch(err => {
-  //     setLoading(false);
-  //     setError(err);
-  //   });
-
-  //   setLoading(false);
-  // };
   return {
     askUserPermission,
     subscribeToPushNotification,
     removeServiceWorker,
-    pushServerId,
     userPermission,
     checkNotificationSupport,
     userSubscription,
