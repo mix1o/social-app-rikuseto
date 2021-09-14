2a3062b5d11f9ef989fd4936f6611ddff2c5fdaf
Mikolaj <smgpwwe@wp.pl>
2021-09-14 17:02:33 +0200

    Remove unused code

--- src/Formik/TextField.tsx
+++ src/Formik/TextField.tsx
@@ -1,5 +1,5 @@
 import { useField } from 'formik';
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import styled from 'styled-components';
 
 interface FieldProps {
