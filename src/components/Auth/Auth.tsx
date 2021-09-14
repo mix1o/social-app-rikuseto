75cdae07f0f5c56b6d8d3c4f942937a65dd03d0d
Mikolaj <smgpwwe@wp.pl>
2021-09-14 17:04:12 +0200

    Removed unused code

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,12 +1,23 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import { useActor } from '@xstate/react';
 import { authService } from './AuthStateMachine';
 import ResetPassword from './ResetPassword';
+import { useCookies } from 'react-cookie';
+import { CookieUser } from '../../interfaces/auth/authInterface';
+import { useHistory } from 'react-router-dom';
 
 const Auth: FC = () => {
   const [current] = useActor(authService);
+
+  const [cookies] = useCookies();
+  const user: CookieUser = cookies['user'] ? { ...cookies['user'] } : undefined;
+  const history = useHistory();
+
+  useEffect(() => {
+    if (user) history.push('/');
+  }, []);
 
   return (
     <div className="auth">
