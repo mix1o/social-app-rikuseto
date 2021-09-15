4a6427d1dc40481f6dac17302df46ae42047d44a
Nightmare9254 <adrian.sarna59@gmail.com>
2021-09-15 19:11:08 +0200

    Removed unused files

--- src/components/Account/IsLogged/isLoggedUser.ts
+++
@@ -1,8 +0,0 @@
-import { CookieUser } from '../../../interfaces/auth/authInterface';
-
-export const checkUser = (id: string | undefined) => {
-  if (!id) {
-    window.location.href = '/auth';
-    return;
-  }
-};
