9a9078d6e7b813673deac95ddfc2bff877b5c48e
Mikolaj <smgpwwe@wp.pl>
2021-12-22 22:38:46 +0100

    Created usePosts hook to fetch all posts

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,39 +1,23 @@
 import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { fetchPosts } from '../api/posts';
 import { PostInterface } from '../interfaces/posts/postInterfaces';
 
 export const usePosts = () => {
-  const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const { isLoading, isError, data, error } = useQuery('posts', fetchPosts, {
+    onSuccess: () => {
+      console.log('Posts fetched'); //For now can be console
+    },
+    onError: () => {
+      throw new Error('Fetching posts failed');
+    },
+  });
 
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      const response = await axios.get(
-        `${process.env.REACT_APP_API}/posts/get`
-      );
-
-      setPosts(response.data);
-      setLoading(false);
-    })();
-  }, []);
-
-  return { posts, loading };
+  return {
+    isLoading,
+    isError,
+    data,
+    error,
+  };
 };
-
-export const sortPosts = () => {
-  return 1;
-};
-
-// const fetchPosts = (): void => {
-//   let url = '';
-
-//   console.log('dupa');
-//   if (postTypes === MODE_ALL) url = '/posts/get';
-//   if (postTypes === MODE_HOME && user)
-//     url = `/posts/get-categories?id=${user._id}`;
-
-//   axios.get(`${process.env.REACT_APP_API}${url}`).then(res => {
-//     setPosts(res.data);
-//   });
-// };
