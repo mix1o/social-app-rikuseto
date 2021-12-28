bfa3a473c9a8df7a3dd7d246d4b1beb53a780798
Mikolaj <smgpwwe@wp.pl>
2021-12-28 21:25:06 +0100

    Handling error server, fixed checking if user has liked post or comment

--- src/hooks/useLike.ts
+++ src/hooks/useLike.ts
@@ -1,42 +1,57 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
-import { useMutation } from 'react-query';
+import { LikedElement } from '../enums/LikedElement';
+import debounce from 'lodash/debounce';
 
-export const useLikeButton = (likes: string[], postId: string) => {
+export const useLikeButton = (
+  likes: string[],
+  id: string,
+  type: LikedElement
+) => {
   const [cookies] = useCookies();
   const { user } = cookies;
 
-  const [isLiked, setIsLiked] = useState<boolean>();
+  const [isLiked, setIsLiked] = useState(false);
   const [likesCounter, setLikesCounter] = useState(likes.length);
 
   useEffect(() => {
-    if (!user && likes.length > 0) return;
+    if (user && likes.length > 0) {
+      const isPostLikedByUser = () => {
+        const index = likes.findIndex((userId: string) => userId === user._id);
+        index === -1 ? setIsLiked(false) : setIsLiked(true);
+      };
 
-    const isPostLikedByUser = () => {
-      const index = likes.findIndex((userId: string) => userId === user._id);
-      index === -1 ? setIsLiked(false) : setIsLiked(true);
-    };
-
-    isPostLikedByUser();
+      isPostLikedByUser();
+    }
   }, [likes, user]);
 
-  const updateLikePost = async () => {
-    try {
-      await axios.post(`${process.env.REACT_APP_API}/posts/like`, {
-        postId,
-        userId: user._id,
-      });
-    } catch (e) {
-      return false;
+  const updateLikePost = useCallback(
+    debounce(async () => {
+      try {
+        //TODO Rename backend id
+        //TODO Implement useMutation
+        await axios.post(`${process.env.REACT_APP_API}/${type}/like`, {
+          [type === LikedElement.Post ? 'postId' : '_id']: id,
+          userId: user._id,
+        });
+      } catch (e) {
+        setLikesCounter(likesCounter);
+        setIsLiked(prevState => !prevState);
+        throw new Error('Ups...'); //TODO: Snackbar component
+      }
+    }, 1000),
+    []
+  );
+
+  const handleLikePost = async () => {
+    if (!user) {
+      alert('You need to log in');
+      return;
     }
-  };
 
-  const likePost = async () => {
-    if (!user) return;
-
-    if (!isLiked) setLikesCounter(likesCounter + 1);
-    if (isLiked) setLikesCounter(likesCounter - 1);
+    if (!isLiked) setLikesCounter(oldLike => oldLike + 1);
+    if (isLiked) setLikesCounter(oldLike => oldLike - 1);
 
     setIsLiked(prevState => !prevState);
 
@@ -44,7 +59,7 @@ export const useLikeButton = (likes: string[], postId: string) => {
   };
 
   return {
-    likePost,
+    handleLikePost,
     isLiked,
     likesCounter,
   };
